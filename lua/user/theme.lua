71eff31898b3397b66bff40841bbd94e6e1db645
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2022-06-18 19:53:41 +0200

    install github theme through safe require

--- lua/user/theme.lua
+++ lua/user/theme.lua
@@ -1,4 +1,9 @@
-require("github-theme").setup({
+local status_ok, githubTheme = pcall(require, "github-theme")
+if not status_ok then
+  return
+end
+
+githubTheme.setup({
   theme_style = "dark",
   sidebars = {"qf", "vista_kind", "terminal", "packer"},
   colors = {hint = "orange", error = "#ff0000"},
