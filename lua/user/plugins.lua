bb63e3ac4e0a470bf26e275b81513d29de2f2d83
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2022-06-19 11:08:20 +0200

    install TS playground

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -89,6 +89,7 @@ return packer.startup(function(use)
     "nvim-treesitter/nvim-treesitter",
     commit = "518e27589c0463af15463c9d675c65e464efc2fe",
   }
+	use { "nvim-treesitter/playground" } -- playground for debugging syntax highlighting
 
   -- Git
   use { "lewis6991/gitsigns.nvim", commit = "c18e016864c92ecf9775abea1baaa161c28082c3" }
