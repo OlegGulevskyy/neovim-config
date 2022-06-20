7e78ba91894e45c32f918f2f41e744e2e443e87a
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2022-06-20 11:38:32 +0200

    keymaps and vertical terminal

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -4,15 +4,15 @@ if not status_ok then
 end
 
 toggleterm.setup({
-	size = 20,
-	open_mapping = [[<c-\>]],
+	size = 60,
+	open_mapping = [[<M-j>]],
 	hide_numbers = true,
 	shade_terminals = true,
 	shading_factor = 2,
 	start_in_insert = true,
 	insert_mappings = true,
 	persist_size = true,
-	direction = "float",
+	direction = "vertical",
 	close_on_exit = true,
 	shell = vim.o.shell,
 	float_opts = {
