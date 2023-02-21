f54451ba6aa12b6915d9b2ef5ebaa544e2a3bca8
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2023-02-21 15:34:26 +0100

    git signs mapping to navigate hunks

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -20,4 +20,29 @@ gitsigns.setup {
 	},
 	current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
 	max_file_length = 40000,
+	on_attach = function(bufnr)
+		local gs = package.loaded.gitsigns
+		local function map(mode, l, r, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+			vim.keymap.set(mode, l, r, opts)
+		end
+
+		map('n', ']c', function ()
+			if vim.wo.diff then return ']c' end
+			vim.schedule(function()
+				gs.next_hunk()
+			end)
+			return '<Ignore>'
+		end, {expr=true})
+
+		map('n', '[c', function ()
+			if vim.wo.diff then return '[c' end
+			vim.schedule(function()
+				gs.prev_hunk()
+			end)
+			return '<Ignore>'
+		end, {expr=true})
+
+	end,
 }
